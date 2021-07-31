4971d2752a454039a7cf4844a299e6b9d6b29604
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-31 23:12:50 +0100

    Update viewsController.js

--- viewsController.js
+++ viewsController.js
@@ -9,20 +9,19 @@ const Booking=require('./bookingModel')
 
 exports.getOverview=catchAsync(async (req,res)=>{
     const tours=await Tour.find();
-    res.status(200).render('toursoverview',{ //title & tours will be passed onto overview template/pug (from db)
+    res.status(200).render('toursoverview',{ 
         title:'All Tours',
-        tours //this is an array containing multiple documents
+        tours 
     })
 })
 
-exports.getTour=catchAsync(async (req,res,next)=>{ // /tours/:slug //just like getATour API following /tours/:id path in tourHandler &tourRouter
-    //const tour=await Tour.findOne({slug:req.params.slug}).populate({
+exports.getTour=catchAsync(async (req,res,next)=>{ 
     const tour=await Tour.findById({_id:req.params.id}).populate({
-            path:'reviews',//virtual field name reviews
-            fields:'review rating user' //fields of Review (model) we need
+            path:'reviews',
+            fields:'review rating user' 
         })
 
-    if(!tour){return next(new appErr("There exists no such tour",404))} //for rendering error page S192
+    if(!tour){return next(new appErr("There exists no such tour",404))} 
 
     res.status(200).render('tourdetails',{
         title: `${tour.name} Tour`,
@@ -38,36 +37,33 @@ exports.getLoginForm=(req,res)=>{
     })
 }
 
-exports.getAccount=(req,res)=>{ //just render the page; no need to query the user as this has been done by auth protect MW
+exports.getAccount=(req,res)=>{ 
     res.status(200).render('account',{
         title: 'Your account'
     })
 } // '/me' render account page
 
-exports.updateUserData=catchAsync(async (req,res)=>{ console.log('id- ',req.user.id,'req.body - ',req.body);
-    console.log('UPDATING DATA...');
-    const updatedUser=await User.findByIdAndUpdate(req.user.id,{ //name attributes in html in account.pug
+exports.updateUserData=catchAsync(async (req,res)=>{ 
+    const updatedUser=await User.findByIdAndUpdate(req.user.id,{ 
         name:req.body.name,
         email:req.body.email
     }, {
         new:true,
         runValidators:true
-    }); //find&update (default no checks) now check validators (both built-in 'require' and custum validator) for name&email(User model fields) - not for other fields; //not run pre-save MW etc
-    res.status(200).render('account',{ //re-render account page using new updatedUser page (instead of using auth protect req.user data)
+    }); 
+    res.status(200).render('account',{ 
         title: 'Your account',
         user:updatedUser
     })
-}) //'/submit-user-data' render account page with new user data #{user} in html/pug
+}) 
 
 
-exports.getMyTours=catchAsync(async (req,res,next)=>{// or using virtual pop?
-    //find all the bookingsfor the cur users -> gives tour iDs
+exports.getMyTours=catchAsync(async (req,res,next)=>{
     const bookings=await Booking.find({user:req.user.id}); //._id
-    //all bookings docs with cur users // all docs containing tour IDs? not populate tour?
 
     //find tours with those returned IDs
-    const tourIDs=bookings.map(el=>el.tour);//create new array based on cb fn// el.tour.id? //el.tour._id? //tour itself is tourId; tour populated when query find?
-    const tours=await Tour.find({_id:{$in:tourIDs}}); // select/find tours with Ids in tourIDs array
+    const tourIDs=bookings.map(el=>el.tour);
+    const tours=await Tour.find({_id:{$in:tourIDs}}); 
 
     res.status(200).render('toursoverview',{
         title:'My Tours',
