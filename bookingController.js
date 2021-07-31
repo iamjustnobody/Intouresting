e22b61612ac853d26542293a70a53527edc11bd0
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-31 19:38:31 +0100

    Update bookingController.js

--- bookingController.js
+++ bookingController.js
@@ -8,20 +8,16 @@ const Booking=require('./bookingModel')
 
 exports.getCheckoutSession=catchAsync(async (req,res,next)=>{ //createCheckoutSession & return session
     //get current tour
-    const tour=await Tour.findById(req.params.tourId); //bookingRoutes router path '/checkout-session/:tourId'
-    console.log('secret type ',typeof process.env.STRIPE_SECRET_KEY);//string //STRIPE_SECRET_KEY=xx or 'xx' both ok string
+    const tour=await Tour.findById(req.params.tourId); 
     //create checkout session
     const stripeSKey=stripe(process.env.STRIPE_SECRET_KEY)
     const session = await stripeSKey.checkout.sessions.create({
         payment_method_types:["card"],
-    //    success_url:`${req.protocol}://${req.get('host')}/`,
         success_url:`${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,//&user=${req.user.id} //for createBookingCheckout
-        cancel_url:`${req.protocol}://${req.get('host')}/tour/${tour.id}`, //or /${tour.id} ok or ${req.params.tourId}
-        // `${req.protocol}//:${req.get('host')}/tour/${tour.name}`, //should be ok as shown in viewRoutes.js & touroverview.pug //or ${tour.slug}
+        cancel_url:`${req.protocol}://${req.get('host')}/tour/${tour.id}`, 
         customer_email:req.user.email,
         client_reference_id: req.params.tourId,
-        //once purchase is successful we'll get access to the session obj again adn then we'll create a new booking in our db -> need user's id, tour's id, & price to create booking
-        line_items:[//an array of objects about the product/tour; one obj per item/product/tour; one tour/product here the user's going to purchase
+        line_items:[
             {
                 name:`${tour.name} Tour`,
                 description: tour.summary,
@@ -33,11 +29,6 @@ exports.getCheckoutSession=catchAsync(async (req,res,next)=>{ //createCheckoutSe
         ]
     })
 
-    //once deploed on server & the purchase's completed, we'll get access to the session object using stripe wehbook to create a new booking
-    //wrokaround in the devleopment mode: put the data we need in the success url as a query string
-    //& strip will just make GET request to success url
-
-
     //create session as response - send session back to client
     res.status(200).json({
         status:'success',
@@ -45,41 +36,28 @@ exports.getCheckoutSession=catchAsync(async (req,res,next)=>{ //createCheckoutSe
     })
 })
 
-exports.createBookingCheckout=catchAsync(async(req,res,next)=>{ //temperory everyone can book without paying if knowing query string
-    const {tour,user,price}=req.query;//not req.params
-    if(!tour&&!user&&!price){return next();}//create booking on success url (home page); success url is called when purchase is successful with stripe
-    //so add this MW to MW stack get('/') in bookigroutes //must return next here or use else below
-    console.log('to create a new booking doc')
-    await Booking.create({tour,user,price}); //checks type/required/validate/pre/post-save MW
-    console.log('complete creating a new booking doc')
-    //next(); //but next next on MW stack is getOverview in viewsController -> render page with success url with all query data
-    //so need to re-move query from the success url
-    res.redirect(req.originalUrl.split('?')[0]); //home page route url '/'  //viewRoutes
-    // redirect creates a new request to the new url (success url without query) `${req.protocol}://${req.get('host')}/`
+exports.createBookingCheckout=catchAsync(async(req,res,next)=>{ 
+    const {tour,user,price}=req.query;
+    if(!tour&&!user&&!price){return next();}
+    await Booking.create({tour,user,price}); 
+    res.redirect(req.originalUrl.split('?')[0]); 
 })
-//suppose this above function could also be placed in viewsController.js then exported to viewRoutes.js
 
-exports.getMyBookings_backend=catchAsync(async (req,res,next)=>{// or using virtual pop?
+exports.getMyBookings_backend=catchAsync(async (req,res,next)=>{
     //find all the bookingsfor the cur users -> gives tour iDs
-    const bookings=await Booking.find({user:req.user.id}); //._id or id both ok
-    //all bookings docs with cur users // all docs containing tour IDs? not populate tour?
+    const bookings=await Booking.find({user:req.user.id}); 
 
     res.status(200).json({
         status:'success',
         data:{bookings}
     })
 })
-exports.getMyTours_backend=catchAsync(async (req,res,next)=>{// or using virtual pop?
-    //find all the bookingsfor the cur users -> gives tour iDs
-    const bookings=await Booking.find({user:req.user._id}); //._id or .id both ok
-    //all bookings docs with cur users // all docs containing tour IDs? not populate tour?
+exports.getMyTours_backend=catchAsync(async (req,res,next)=>{
+    const bookings=await Booking.find({user:req.user._id}); 
 
     //find tours with those returned IDs
-    const tourIDs=bookings.map(el=>el.tour);//create new array based on cb fn// el.tour.id? //el.tour._id? //tour itself is tourId; tour populated when query find?
-    console.log(tourIDs);//el=>el.tour o/p: complete POPULATED tour [{xx},{yy},{ww}]
-    //el=>el.tour._id o/p: [xxid,yyid,wwid]; //el=>el.tour.id o/p:['xxid','yyid','wwid']
-    //all above three ok
-    const tours=await Tour.find({_id:{$in:tourIDs}}); // select/find tours with Ids in tourIDs array
+    const tourIDs=bookings.map(el=>el.tour);
+    const tours=await Tour.find({_id:{$in:tourIDs}}); 
 
     res.status(200).json({
         status:'success',
