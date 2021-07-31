c9c0ccafc2d2f815bf25997614154d59424a021c
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-31 20:01:56 +0100

    Update errorHandler.js

--- errorHandler.js
+++ errorHandler.js
@@ -1,21 +1,4 @@
-/*module.exports=(err,req,res,next)=>{
-    err.statusCode=err.statusCode||500;
-    err.status=err.status||'error';
-    console.log(err.stack);
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    })
-};//this code is ok but now we need to think about errors during development vs production
-*/
 
-//global error last error in app.js
-//apperror (from mongodb id/tour existence error from tourhandler or from appjs for invalid route path); or try catch async error
-//operational error isOperational -> wrong route path (AppError or user input /:id req) or user input invalid data (validator error)
-//need to make some more errors isoperational (other than those created from new appError())
-//3 types of operational errors from mongoose: invalid id; duplicate key error, validation error
-
-//the above code is ok but now we need to think about errors during development vs production
 const appErr=require('./Utils/AppError');
 const sendErrDev=(res, err)=>{  //API
      res.status(err.statusCode).json({
@@ -40,9 +23,9 @@ const sendErrDev2=(req,res, err)=>{
             message:err.message,
             stack:err.stack
         })
-    }//else { //above if already returned
+    }
         //RENDERED WEBSITE
-        res.status(err.statusCode).render('error',{ //could return here as well
+        res.status(err.statusCode).render('error',{
             title:'Something went wrong',
             msg:err.message
         })
@@ -72,13 +55,13 @@ const sendErrProd2=(err,res)=>{
             title:'Something went wrong',
             msg:err.message
         })
-    }//else{ //above if already returned
+    }
         console.error('ERROR',err); //for developer
-        res.status(err.statusCode).render('error',{  //could return here
+        res.status(err.statusCode).render('error',{  
             title:'Something went wrong',
             msg:"Please try again later"
         })
-    //}
+   
 
 }
 
@@ -107,29 +90,25 @@ const handleTokenExpErr = () => new appErr("Token expired. Please log in again",
 module.exports=(err,req,res,next)=>{
     err.statusCode=err.statusCode||500;
     err.status=err.status||'error';
-    console.log('we are in the mode of ',process.env.NODE_ENV, typeof process.env.NODE_ENV);
-    console.log(err.stack);
-    if(process.env.NODE_ENV.trim()==='development'){ console.log("we are in the development mode now");
+    //console.log('we are in the mode of ',process.env.NODE_ENV, typeof process.env.NODE_ENV);
+    //console.log(err.stack);
+    if(process.env.NODE_ENV.trim()==='development'){ //console.log("we are in the development mode now");
         sendErrDev2(req,res,err); //or return sendErrDev2(req,res,err); //sendErrDev(res,err);//original api
-       // if(req.originalUrl.startsWith('/api')){return sendErrDev(res,err);}  sendErrDev2(req,res,err);  //ok
-        // if(req.originalUrl.startsWith('/api')){return sendErrDev(res,err);}  return sendErrDev2(req,res,err); //ok
     }else if (process.env.NODE_ENV.trim()==='production'){ console.log("we are in the production mode now");
       //  sendErrProd(err,res);//ok but also need to make more mongo errors operational as shown below
         let error={...err};
 
-        console.log("original err message ", err.message," copied error message ", error.message);
+        //console.log("original err message ", err.message," copied error message ", error.message);
         error.message=err.message;
 
         if(err.name==='CastError'){error=handleCastErrDB(error);} //or error.name or handleCastErrDB(err); but needs error as left assignee and argument for sendErrProd below
         if(err.code===11000){error=handleDupFieldsErrDB(error);}
         if(err.name==='ValidationError'){error=handleValidationErrDB(error);}//ok
-       // if(err.message.substr(13,23)==='Validation'){error=handleValidationErrDB(error);}//not 100% correct & substr index too dependable/ may vary
 
         if(err.name==='JsonWebTokenError') error=handleJWTErr();
         if(err.name==='TokenExpiredError') error=handleTokenExpErr();
-        //above making/marking more operational errors
+        
         if(req.originalUrl.startsWith('/api')){sendErrProd(error,res);} else {sendErrProd2(error,res);}//sendErrProd(error,res); //original api
-        //if(req.originalUrl.startsWith('/api')){return sendErrProd(error,res);}  sendErrProd2(error,res); //?
-    } //somehow NODE_ENV not working? (cannot switch to production) why?
-   // sendErrDev(res,err);
+       
+    } 
 };
