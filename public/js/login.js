e3f4fb6b79eff2f4a440beed9af412d545dba12e
iamjustnobody <76815821+iamjustnobody@users.noreply.github.com>
2021-07-31 23:25:39 +0100

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,10 @@
-//import -> type module in public package.json or mjs; frontend-> script type module
-//as comment out script cdn axios.min.js so need to import
-//import axios from 'axios'; //not display map // but need ./ reference
-//import axios from './node_modules/axios';
-//or get net err aborted 404 error tring to get from node_model
-//need boundle or webpack & script boundle.js in index.mjs
-import axios from "axios"; //parcel require not defined & not showing map
-//remove package json in public folder & return mjs to js
+
+import axios from "axios"; 
 
 import {showAlert} from "./alerts";
 
-//export const login=async (email,password)=>{
-export async function login (email,password){ console.log(email,password); //for testing but usually not logging sensitive data to console
-//http request for login //cdn or npm axios
+
+export async function login (email,password){ 
     try{
         const res=await axios({
             method:'POST',
@@ -21,31 +14,17 @@ export async function login (email,password){ console.log(email,password); //for
                 password
             }
         });
-        console.log(res); //res.data is our data response - json
         if(res.data.status==='success'){
-            //alert('Logged in successfully!');//ok
             showAlert('success', 'Logged in successfully!');
             window.setTimeout(()=>{
-                location.assign('/');//location.reload(true); but deprecated (so plus window.) but also dont wanna stay in current login page so redirect to home page
+                location.assign('/');
             },1500);
         }
     }catch(err){
-        //  console.log(err.response.data);//err.response.data- data is our data response - json
-        //err.response.data in axios documentation
-        // alert(err.response.data.message);
-       console.log(err);
        showAlert('error',err.response.data.message);
     }
 
 }
-
-/*document.querySelector('.form').addEventListener('submit',e=>{
-    e.preventDefault();
-    const email=document.getElementById('email').value;
-    const password=document.getElementById('password').value;
-    login(email,password);
-})*/ //move to index.mjs now index.js
-//move to index that gets user interface & delegate actions
 
 
 export async function logout(){
@@ -54,10 +33,8 @@ export async function logout(){
             method:'GET',
             url:'http://localhost:3001/api/v1/users/logout',
         });
-        console.log("to log out")
-      //  if(res.data.status==='success') window.location.reload(true) //= or == or === all seem ok; === definitely ok
-        //but if in '/me' page when click logout jwt cookie='loggedout' if stay at /me page this cookie wont pass auth protect promise verify so jwt mal error
-        if(res.data.status==='success'){window.setTimeout(()=>{location.assign('/');},1500);} //so when logout redirect to home page
+
+        if(res.data.status==='success'){window.setTimeout(()=>{location.assign('/');},1500);} 
     }catch(err){
         showAlert("error","Error logging out! Please try it again.")
     }
